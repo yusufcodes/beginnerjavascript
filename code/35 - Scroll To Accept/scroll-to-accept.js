1686330b04faf59f9fbe795f84e8d0eee724bfbe
yusufcodes <yusufcodes@outlook.com>
2020-03-03 14:54:02 +0000

    Completed Intersection Observer Task Learned about: - Using the scroll event - Using an IntersectionObserver and properties on it to detect when an element has been scrolled over - Adding/removing a HTML attribute in order to hide/show the element using disabled

--- code/35 - Scroll To Accept/scroll-to-accept.js
+++ code/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,38 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+
+terms.addEventListener('scroll', function(event) {
+  //   console.log(event.currentTarget.scrollTop);
+  //   console.log(event);
+  //   if (event.currentTarget.scrollTop > 1600) {
+  //     console.log('User can now accept the terms?');
+  //     document.querySelector('.accept').removeAttribute('disabled');
+  //   }
+
+  // Intersection Observer: watches if an element is on / slight on the page
+  const options = {
+    root: terms,
+    threshold: 0.5,
+  };
+
+  const handleOverlap = function(payload) {
+    // Display the Accept button to the user
+    // console.log(payload[0].intersectionRatio);
+    if (payload[0].intersectionRatio === 1) {
+      console.log('We can accept the terms!');
+      //   document.querySelector('.accept').removeAttribute('disabled');
+      document.querySelector('.accept').disabled = false;
+    } else {
+      document.querySelector('.accept').disabled = true;
+    }
+  };
+
+  const observer = new IntersectionObserver(handleOverlap, options);
+
+  observer.observe(terms.lastElementChild);
+  //   console.log(terms.lastElementChild);
+
+  /* Steps */
+  // scroll until the user is at the bottom (can we use the closest method on each scroll until we are close to the accept button? )
+
+  // make the accept button clickable
+});
