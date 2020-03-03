0d180dd0cd8ccb196fc62645398fd187ed6a4c22
yusufcodes <yusufcodes@outlook.com>
2020-03-03 15:48:49 +0000

    Completed the tabs exercise Learned about: - Using aria properties linked to css to contro the actions of tabs - Quick recap of destructuring - Using Array.from() to convert a NodeList into an Array to be able to access typical methods from an array

--- code/36 - Tabs/tabs.js
+++ code/36 - Tabs/tabs.js
@@ -1,1 +1,30 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]')); // Turn NodeList -> Array to be able to use array method on it (find)
+
+function handleTabClick(event) {
+  // Hide all of the panels first
+  tabPanels.forEach(panel => {
+    panel.hidden = true;
+  });
+
+  // Mark all tabs as unselected using a HTML attribute
+  tabButtons.forEach(tab => {
+    tab.setAttribute('aria-selected', false);
+  });
+
+  // Mark the clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+
+  // Get the Id of the tab clicked on
+  const { id } = event.currentTarget;
+  // Same as: const id = event.currentTarget.id, called destructuring
+  const tabPanel = tabPanels.find(
+    panel => panel.getAttribute('aria-labelledby') === id
+  );
+
+  // Display the panel by setting the hidden attribute to false
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
